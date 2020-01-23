d7536d5c2877fcb871bef36557a5049f02bf19e8
Sherry <50695343+yu-lin42@users.noreply.github.com>
2020-01-23 10:30:28 +0200

    with int and char

--- dereference.c
+++ dereference.c
@@ -4,7 +4,7 @@ void	print_c(char t);
 void	print_a(char *test);
 void	print_n(int q);
 
-int main(void)
+/*int main(void)
 {
 	int apple;
 	int *pointer_apple;
@@ -17,5 +17,21 @@ int main(void)
 	print_c('\n');
 	print_a("pointer_apple\t");
 	print_n(*pointer_apple);
+	print_c('\n');
+}*/
+
+int main(void)
+{
+	char apple;
+	char *pointer_apple;
+
+	apple = 'c';
+	pointer_apple = &apple;
+	print_a("Variable\tWhat value it holds\n");
+	print_a("apple\t\t");
+	print_c(apple);
+	print_c('\n');
+	print_a("pointer_apple\t");
+	print_c(*pointer_apple);
 	print_c('\n');
 }
